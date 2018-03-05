1c9a5cf30b973f38cfa95ebc92984921b402004a
aishwat <aishwat.singh@target.com>
2018-03-05 16:34:16 +0530

    module_9

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+import numeral from 'numeral';
+
+
+const courseVal = numeral(1000).format('$0,0.00'); //eslint-disable-line no-unused-vars
+// debugger;
+// console.log(`I would pay ${courseVal} for this awesome course!`);
